fbf473f3f7da426bcdc149b862c9626743b6d4d3
Deepesh Kumar <121308316+deepeshkumar06@users.noreply.github.com>
2024-03-06 15:19:23 +0530

    Add files via upload

---
+++ college/6-3-2024/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form'
+import DtI from './components/DtI';
+import Calculator from './components/Calculator';
+import List from './components/List';
+import FormTable from './components/FormTable';
+
+
+function App() {
+  return (
+    // <Form />
+    // <DtI />
+    // <Calculator />
+    // <List />
+    <FormTable />
+  );
+}
+
+export default App;
